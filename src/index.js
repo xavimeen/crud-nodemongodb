4ba94cbeef4719125ee18fa00cbec0465803d3f2
Xavi <xaviermendoza.c@outlook.com>
2020-08-11 02:01:42 -0300

    Added a Procfile.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 const app = require('./server');
 require('./database');
 
-app.listen(app.get('port'), () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Servidor en el puerto: ${app.get('port')}`);
 });
